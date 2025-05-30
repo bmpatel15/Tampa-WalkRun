b2e7b853f428fc5079dd7a325187fd4806f7b825
Bhavesh <bpatel501@gmail.com>
2025-05-30 19:31:51 -0400

    fixed some deployment errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
+import next from 'eslint-plugin-next';
+
 export default [
   {
     ignores: [
       'node_modules/',
       'lib/generated/',
     ],
+    plugins: {
+      next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+    },
   },
   // Add your other ESLint config objects below if needed
 ]; 
